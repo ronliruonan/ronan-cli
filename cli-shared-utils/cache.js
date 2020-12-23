0eab2fe8c0e0bfa1c3d27cebb2b0d6b80c25954f
Ronan <liruonan@hotmail.com>
2020-12-23 14:07:45 +0800

    refactor(cache): 优化cache 判断逻辑，在无文件时自动创建

--- cli-shared-utils/cache.js
+++ cli-shared-utils/cache.js
@@ -1,20 +1,41 @@
 const shelljs = require('shelljs');
-const { textRedBright, textCyan } = require('../cli-shared-utils/chalk');
+const { textRedBright, textCyan, textGreen } = require('../cli-shared-utils/chalk');
 // 文件模块
+const fs = require('fs');
 const path = require('path');
 // project json
 const __path__ = path.resolve(__dirname, '../cache/local-projects.json');
-const __projects__ = require(__path__); // []
+
+/**
+ * projects 缓存路径
+ */
+const cachePath = (() => {
+  if (fs.existsSync(__path__)) return __path__;
+
+  fs.writeFileSync(__path__, JSON.stringify([]));
+
+  return __path__;
+})();
+
+/**
+ * projects 缓存数据
+ */
+const cacheProjects = require(cachePath);
+
+/**
+ * project 判空后的数据
+ */
+const  cacheProjectsValid = () => {
+  if (cacheProjects.length < 1) {
+    console.log(textRedBright('cache projects is empty,'), 'please `ro add`');
+    shelljs.exit(1);
+  }
+
+  return cacheProjects;
+}
 
 module.exports = {
-  cachePath: __path__,
-  cacheProjects: __projects__,
-  cacheProjectsValid: () => {
-    if (__projects__.length < 1) {
-      console.log(textRedBright('cache projects is empty,'), 'please `ro add`');
-      shelljs.exit(1);
-    }
-
-    return __projects__;
-  }
+  cachePath,
+  cacheProjects,
+  cacheProjectsValid,
 };
---
+++ tests/test.space.js
@@ -0,0 +1,4 @@
+const { textCyan } = require('../cli-shared-utils/chalk');
+const { cachePath } = require('../cli-shared-utils/cache');
+
+console.log(textCyan(cachePath));
