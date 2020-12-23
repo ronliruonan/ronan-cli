1cf13362a6a5603d8549566dbcaf02fc2218476d
Ronan <liruonan@hotmail.com>
2020-12-23 18:17:23 +0800

    test:

--- tests/test.space.js
+++ tests/test.space.js
@@ -1,4 +1,7 @@
 const { textCyan } = require('../cli-shared-utils/chalk');
 const { cachePath } = require('../cli-shared-utils/cache');
+const chalk = require('chalk');
 
 console.log(textCyan(cachePath));
+
+console.log(chalk.gray('asdfasdfasdf'))
